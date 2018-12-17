94f1b99471796e56b9db120016a2af8cad2d9ea1
justus.leichtweiss <justus.leichtweiss@R001-20.hgw.local>
2018-12-17 13:02:27 +0100

    Ein paar Änderungen und import der ersten Images

--- src/Einzelspieler/GameGUI.java
+++ src/Einzelspieler/GameGUI.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class GameGUI extends JFrame {
 
@@ -32,11 +34,18 @@ public class GameGUI extends JFrame {
 	 */
 	public GameGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 951, 630);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnZurck = new JButton("Zur\u00FCck");
+		btnZurck.setBounds(10, 11, 83, 23);
+		contentPane.add(btnZurck);
+		
+		JLabel FrogUI = new JLabel("");
+		FrogUI.setBounds(425, 520, 46, 14);
+		contentPane.add(FrogUI);
 	}
-
 }
